e42ff186a531ec08da359dbb3e9785795447f987
on-stu <minsu0523@naver.com>
2022-06-06 23:34:08 +0900

    key

--- context/socket.js
+++ context/socket.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:3000/");
+export const socket = io("http://3.34.1.175:3000/");
 const SocketContext = createContext(socket);
 
 export default SocketContext;
